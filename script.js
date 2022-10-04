66c61c8d10ee6993895ddc577158cd684a8ff144
omsomni <97291868+omsomni@users.noreply.github.com>
2022-10-04 20:38:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,12 @@ const pages = {
     "Marami pa akong ilalagay here and mga additional something.",
     " ",
     "Gagawin ko to as parang diary kasi why not. Lalagay ko rin dito mga thoughts ko na hindi ko kaya sabihin sayo mwehehe",
-    ]
+  ],
+  6: [
+    "10/04/2022",
+    "Grabe ka na lods nagwawax ka na rin ba ng mukha mo",
+    "Parehas na kayo ni kaila kumikintab na mukha HAHAHAHHAHH"
+  ]
 };
 
 // a cycle of gwen's pics
